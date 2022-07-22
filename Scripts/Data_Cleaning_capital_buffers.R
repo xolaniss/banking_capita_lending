3cf2478a3d3aa3d12692a599f66ae04da456b6a0
Xolani Sibande <xolaniss@gmail.com>
2022-07-22 15:09:27 +0200

    capital buffers graphs

--- Scripts/Data_Cleaning_capital_buffers.R
+++ Scripts/Data_Cleaning_capital_buffers.R
@@ -24,50 +24,67 @@ library(fDMA)
 source(here("Functions", "fx_plot.R"))
 
 # Import ------------------------------------------------------------------
-capital_buffers <- read_excel(here("Data", "capital_buffers.xlsx"), skip = 3 )
+capital_buffers <-
+  read_excel(here("Data", "capital_buffers.xlsx"), skip = 3)
 
 
 # Cleaning ----------------------------------------------------------
-capital_buffers_tbl <- 
-  capital_buffers %>% 
-  relocate(...3, .before = ...1) %>% 
-  rename("Bank" = ...1, 
-         "Date" = ...3,
-         `Aggregate amount of qualifying capital and reserve funds` =
-           `Aggregate amount of qualifying capital and reserve funds...9`) %>% 
-  fill(Bank, .direction = "down") %>% 
-  dplyr::select(-...2, -...4) %>% 
-  mutate(Date = parse_date_time(Date, orders = "bY")) %>% 
-  dplyr::select(Date, Bank, `Minimum required capital and reserve funds`, 
-                `Aggregate amount of qualifying capital and reserve funds`,
-                `Surplus capital`)
+capital_buffers_tbl <-
+  capital_buffers %>%
+  relocate(...3, .before = ...1) %>%
+  rename(
+    "Bank" = ...1,
+    "Date" = ...3,
+    `Aggregate amount of qualifying capital and reserve funds` =
+      `Aggregate amount of qualifying capital and reserve funds...9`
+  ) %>%
+  fill(Bank, .direction = "down") %>%
+  dplyr::select(-...2, -...4) %>%
+  mutate(Date = parse_date_time(Date, orders = "bY")) %>%
+  dplyr::select(
+    Date,
+    Bank,
+    `Minimum required capital and reserve funds`,
+    `Aggregate amount of qualifying capital and reserve funds`,
+    `Surplus capital`
+  ) %>% 
+  drop_na() %>% 
+  mutate(Bank = str_replace_all(Bank, "THE STANDARD BANK OF S A LTD", "STANDARD BANK")) %>% 
+  mutate(Bank = str_replace_all(Bank, "FIRSTRAND BANK LIMITED", "FIRSTRAND BANK")) %>% 
+  mutate(Bank = str_replace_all(Bank, "ABSA BANK LTD", "ABSA BANK")) %>% 
+  mutate(Bank = str_replace_all(Bank, "NEDBANK LTD" , "NEDBANK" ))
 
 # Graphing ----------------------------------------------------------------
 bank_names <- list(unique(capital_buffers_tbl$Bank))
 
 bank_capital_buffer_gg <- function(data, variables_color = 3, ...) {
- ggplot <-  data %>% 
-    pivot_longer(cols = -(1:2), values_to = "Value", names_to = "Series") %>% 
-    filter(Bank %in% ...) %>% 
-    unite("Series", Bank:Series, sep = ": ") %>% 
+  ggplot <-  data %>%
+    pivot_longer(cols = -(1:2),
+                 values_to = "Value",
+                 names_to = "Series") %>%
+    filter(Bank %in% ...) %>%
+    unite("Series", Bank:Series, sep = ": ") %>%
     fx_nopivot_plot(variables_color = variables_color, scale = "free") +
     scale_y_continuous(labels = scales::label_percent())
- ggplot
+  ggplot
 }
 
-big_banks <- c("ABSA BANK LTD", 
-               "CAPITEC BANK", 
-               "DISCOVERY BANK LIMITED" ,
-               "FIRSTRAND BANK LIMITED",
-               "INVESTEC BANK LTD",
-               "NEDBANK LTD" ,
-               "THE STANDARD BANK OF S A LTD" )
+big_banks <- c(
+  "ABSA BANK ",
+  "CAPITEC BANK",
+  "FIRSTRAND BANK",
+  "NEDBANK" ,
+  "STANDARD BANK"
+)
 
-bank_capital_buffer_gg(capital_buffers_tbl, variables_color = 21,
-                  big_banks)
+
+big_banks_gg <- 
+  bank_capital_buffer_gg(capital_buffers_tbl,
+                       variables_color = 12,
+                       big_banks)
 
 # Export ------------------------------------------------------------------
-artifacts_capital_buffers <- 
-  list(
-    Data = list(capital_buffers_tbl)
-  ) %>% write_rds(file = here("Outputs", "artifacts_capital_buffers.rds"))
+artifacts_capital_buffers <-
+  list(Data = list(capital_buffers_tbl),
+       Graphs = list(big_banks_gg)) %>% 
+  write_rds(file = here("Outputs", "artifacts_capital_buffers.rds"))
